92b851f7888f77cebf1988ce74345c1035f40cf0
VenaNocta <venanocta@gmail.com>
2026-02-12 21:59:30 +0100

    added dependencies and modules

--- api/src/main/java/net/runeduniverse/lib/repo/maven/proxy/api/RepositoryBuilder.java
+++ api/src/main/java/net/runeduniverse/lib/repo/maven/proxy/api/RepositoryBuilder.java
@@ -14,3 +14,7 @@
  * limitations under the License.
  */
 package net.runeduniverse.lib.repo.maven.proxy.api;
+
+public interface RepositoryBuilder {
+
+}
--- cache-caffeine/src/test/java/net/runeduniverse/lib/repo/maven/proxy/cache/caffeine/test/ExampleTest.java
+++ cache-caffeine/src/test/java/net/runeduniverse/lib/repo/maven/proxy/cache/caffeine/test/ExampleTest.java
@@ -13,4 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.runeduniverse.lib.repo.maven.proxy;
+package net.runeduniverse.lib.repo.maven.proxy.cache.caffeine.test;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+	public void print(String line) {
+		System.out.println(LocalDateTime.now() + ": " + line);
+	}
+
+	@Test
+	@Tag("system")
+	public void exec() throws InterruptedException {
+		print("test example log");
+	}
+
+}
