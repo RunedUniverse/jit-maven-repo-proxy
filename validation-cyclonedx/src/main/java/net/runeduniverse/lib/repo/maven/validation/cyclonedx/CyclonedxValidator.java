510d630392890523872ba4ad5d3ff93c7b892fcd
VenaNocta <venanocta@gmail.com>
2026-03-17 00:02:21 +0100

    implemented validation-cyclonedx

--- validation-cyclonedx/src/main/java/net/runeduniverse/lib/repo/maven/validation/cyclonedx/CyclonedxValidator.java
+++ validation-cyclonedx/src/main/java/net/runeduniverse/lib/repo/maven/validation/cyclonedx/CyclonedxValidator.java
@@ -15,38 +15,137 @@
  */
 package net.runeduniverse.lib.repo.maven.validation.cyclonedx;
 
-import org.cyclonedx.model.Bom;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.cyclonedx.model.Component;
+import org.cyclonedx.model.Hash;
+import org.cyclonedx.model.Property;
 
 import net.runeduniverse.lib.repo.maven.api.ArtifactData;
 import net.runeduniverse.lib.repo.maven.api.ArtifactValidator;
 import net.runeduniverse.lib.repo.maven.error.InvalidArtifactException;
+import net.runeduniverse.lib.repo.maven.error.InvalidChecksumArtifactException;
+import net.runeduniverse.lib.repo.maven.error.SBomViolationException;
 import net.runeduniverse.lib.repo.maven.validation.pgp.PGPArtifactSignatureValidator;
 import net.runeduniverse.lib.repo.maven.validation.pgp.PublicKeyIndex;
 
 public class CyclonedxValidator implements ArtifactValidator {
 
-	protected final Bom bom;
-	protected final PublicKeyIndex keyIndex;
+	public static final String MSG_CHECKSUM_MISSMATCH = "SBOM: Artifact Checksum does not match expected value!";
+	public static final String MSG_SIGNATURE_KEYID_MISSMATCH = "SBOM: Artifact PGP-Signature KeyID does not match expected value!";
+	public static final String MSG_SIGNATURE_FINGERPRINT_MISSMATCH = "SBOM: Artifact PGP-Signature Fingerprint does not match expected value!";
 
-	public CyclonedxValidator(final Bom bom, final PublicKeyIndex keyIndex) {
-		this.bom = bom;
+	protected final PublicKeyIndex keyIndex;
+	protected final ComponentIndex componentIndex;
+
+	protected boolean skipMissing = false;
+
+	public CyclonedxValidator(final PublicKeyIndex keyIndex, final ComponentIndex componentIndex) {
 		this.keyIndex = keyIndex;
+		this.componentIndex = componentIndex;
+	}
+
+	/**
+	 * Set skipMissing components flag.
+	 * <p>
+	 * NOTE: Only use when intending to chain multiple SBOM-Validator, otherwise
+	 * artifacts will slip through the cracks!
+	 *
+	 * @param value {@code true} to disarm the validate() method, prevents throwing
+	 *              of {@link InvalidArtifactException} for missing components, else
+	 *              {@code false}.
+	 */
+	public void setSkipMissing(final boolean value) {
+		this.skipMissing = value;
 	}
 
 	@Override
 	public boolean validate(final ArtifactData data) throws InvalidArtifactException {
 		final PGPArtifactSignatureValidator pgpValidator = new PGPArtifactSignatureValidator(this.keyIndex);
 
-		// TODO check the artifact signature
+		final Component comp = this.componentIndex.getComponentByPURL(data.getPURL());
+		if (comp == null) {
+			if (this.skipMissing)
+				return false;
+			else
+				throw new SBomViolationException("SBOM: Artifact not listed in SBOM");
+		}
 
+		// -- create Property-Map
+		final Map<String, String> properties = new LinkedHashMap<>();
+		{
+			final List<Property> propList = comp.getProperties();
+			if (propList != null) {
+				for (Property property : propList) {
+					final String key = StringUtils.trimToNull(property.getName());
+					if (key == null)
+						continue;
+					properties.put(key, property.getValue());
+				}
+			}
+		}
+
+		// -- verify checksums, if available
+		final Collection<Hash> hashes = comp.getHashes();
+		if (hashes != null && !hashes.isEmpty()) {
+			final Map<String, String> dataHashes = data.getChecksums();
+
+			for (Hash hash : hashes) {
+				final String algorithm = hash.getAlgorithm();
+				final String localChecksum = dataHashes.get(algorithm);
+				final String remoteChecksum = hash.getValue();
+				if (!localChecksum.equalsIgnoreCase(remoteChecksum)) {
+					throw new InvalidChecksumArtifactException(MSG_CHECKSUM_MISSMATCH, algorithm, localChecksum,
+							remoteChecksum);
+				}
+			}
+		}
+
+		// -- verify artifact signature, if available
 		if (pgpValidator.validate(data)) {
 			// if the signature exists
 			// -> fails if -> throws error -> accepted
 			// -> if true, try to check the signature against the SBOM
+			final String pgpKeyID = properties.get("pgp:keyId");
+			final String pgpFingerprint = properties.get("pgp:fingerprint");
 
-			// TODO check the signature against the SBOM
+			if (pgpKeyID != null || pgpFingerprint != null) {
+				// TODO check the signature against the SBOM
+
+				try {
+					final PGPSignature pgpSignature = pgpValidator.getSignature(data.getSignaturePath());
+					final long keyID = pgpSignature.getKeyID();
+
+					if (pgpKeyID != null) {
+						if (!pgpKeyID.trim()
+								.equalsIgnoreCase(Long.toHexString(keyID)))
+							throw new SBomViolationException(MSG_SIGNATURE_KEYID_MISSMATCH);
+					}
+
+					if (pgpFingerprint != null) {
+						// the key must already be known otherwise pgpValidator.validate(data)
+						// would not be true
+						final PGPPublicKey pubKey = this.keyIndex.getKey(keyID);
+
+						if (!pgpFingerprint.trim()
+								.equalsIgnoreCase(Hex.encodeHexString(pubKey.getFingerprint())))
+							throw new SBomViolationException(MSG_SIGNATURE_FINGERPRINT_MISSMATCH);
+					}
+				} catch (IOException | PGPException e) {
+					throw new SBomViolationException("SBOM: Unexpected Exception occurred when loading PGPSignature",
+							e);
+				}
+			}
 		}
 
-		return false;
+		return true;
 	}
 }
