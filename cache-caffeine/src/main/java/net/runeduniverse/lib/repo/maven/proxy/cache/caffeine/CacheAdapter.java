00473887b3c2ab294eef6bdd10cc410acef3e538
VenaNocta <venanocta@gmail.com>
2026-04-09 20:51:10 +0200

    added cache-caffeine implementation

--- cache-caffeine/src/main/java/net/runeduniverse/lib/repo/maven/proxy/cache/caffeine/CacheAdapter.java
+++ cache-caffeine/src/main/java/net/runeduniverse/lib/repo/maven/proxy/cache/caffeine/CacheAdapter.java
@@ -16,32 +16,50 @@
 package net.runeduniverse.lib.repo.maven.proxy.cache.caffeine;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 import net.runeduniverse.lib.repo.maven.api.ArtifactCoordinates;
 import net.runeduniverse.lib.repo.maven.api.ArtifactData;
 import net.runeduniverse.lib.repo.maven.api.ArtifactDataCoordinates;
 import net.runeduniverse.lib.repo.maven.api.ArtifactMetadata;
 import net.runeduniverse.lib.repo.maven.proxy.api.MavenRepositoryProxyInstance;
+import net.runeduniverse.lib.repo.maven.proxy.api.SourceArtifactData;
 import net.runeduniverse.lib.repo.maven.proxy.cache.api.Cache;
 
 public class CacheAdapter implements Cache {
 
 	protected final MavenRepositoryProxyInstance proxy;
+	protected final AsyncLoadingCache<ArtifactCoordinates, ArtifactMetadata> metadataCache;
+	protected final AsyncLoadingCache<ArtifactDataCoordinates, ArtifactData> artifactCache;
 
 	public CacheAdapter(final MavenRepositoryProxyInstance proxy) {
 		this.proxy = proxy;
+		this.metadataCache = Caffeine.newBuilder()
+				.buildAsync(this::lookupMetadata);
+		this.artifactCache = Caffeine.newBuilder()
+				.buildAsync(this::lookupArtifact);
+	}
+
+	protected CompletableFuture<ArtifactMetadata> lookupMetadata(final ArtifactCoordinates coords,
+			final Executor executor) {
+		return this.proxy.lookupMetadata(coords);
+	}
+
+	protected CompletableFuture<SourceArtifactData> lookupArtifact(final ArtifactDataCoordinates coords,
+			final Executor executor) {
+		return this.proxy.lookupArtifact(null, coords);
 	}
 
 	@Override
-	public CompletableFuture<ArtifactMetadata> getMetadata(ArtifactCoordinates coords) {
-		// TODO implement cache
-		return this.proxy.getMetadata(coords);
+	public CompletableFuture<ArtifactMetadata> getMetadata(final ArtifactCoordinates coords) {
+		return this.metadataCache.get(coords);
 	}
 
 	@Override
-	public CompletableFuture<? extends ArtifactData> getArtifact(ArtifactDataCoordinates coords) {
-		// TODO implement cache
-		return this.proxy.getArtifact(coords);
+	public CompletableFuture<? extends ArtifactData> getArtifact(final ArtifactDataCoordinates coords) {
+		return this.artifactCache.get(coords);
 	}
-
 }
